d1dfb3c31f34c67dbdc5aa2d490248c9b6e94c97
alamin20cse <md.alamin165086@gmail.com>
2024-12-22 01:42:12 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,96 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const app = express();
+const port = process.env.PORT || 4000;
+
+
+
+// middleWare
+app.use(cors());
+app.use(express.json());
+
+
+
+
+
+
+// 
+// 
+
+
+
+
+
+
+
+const uri = `mongodb+srv://${process.env.USER}:${process.env.PASS}@cluster0.uslpn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    // // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
+console.log();
+console.log();
+
+    const tshirtCollection=client.db('tshirtDB').collection('tshirt');
+
+    app.get('/tshirts',async(req,res)=>{
+        const cursor=tshirtCollection.find();
+        const result=await cursor.toArray();
+        res.send(result);
+    })
+
+
+    app.post('/tshirts',async(req,res)=>{
+        const newTshirt=req.body;
+        console.log(newTshirt);
+
+        const result=await tshirtCollection.insertOne(newTshirt);
+        res.send(result);
+
+    })
+
+    app.delete('/tshirts/:id',async(req,res)=>{
+        const id=req.params.id;
+        const query={_id:new ObjectId(id)}
+        const result= await tshirtCollection.deleteOne(query);
+        res.send(result);
+    })
+
+
+
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+   
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
+
+
+
+// Fix the route handler
+app.get('/', (req, res) => {
+    res.send('running');
+});
+
+app.listen(port, () => {
+    console.log(`running on port ${port}`);
+});
